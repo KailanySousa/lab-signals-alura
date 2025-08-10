62d472c9064cf3a5b1e3da2ea3be1b0c153a7933
KailanySousa <kailany-sousa2010@hotmail.com>
2025-08-09 22:17:37 -0300

    feat: cria componente para os elementos favoritos

---
+++ src/app/components/element-list/element-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import Elemento from 'src/app/services/elemento/elemento.interface';
+import { ElementoService } from 'src/app/services/elemento/elemento.service';
+
+@Component({
+  selector: 'app-element-list',
+  templateUrl: './element-list.component.html',
+  styleUrl: './element-list.component.css',
+})
+export class ElementListComponent {
+  constructor(readonly elementoService: ElementoService) {}
+
+  alternarFavorito(elemento: Elemento) {
+    const isFavorito = this.elementoService.favoritos().includes(elemento);
+    if (isFavorito) {
+      this.elementoService.removerFavorito(elemento);
+    } else {
+      this.elementoService.adicionarFavorito(elemento);
+    }
+  }
+
+  adicionarFavorito(elemento: Elemento) {
+    this.elementoService.adicionarFavorito(elemento);
+  }
+
+  removerFavorito(elemento: Elemento) {
+    this.elementoService.removerFavorito(elemento);
+  }
+}
