0fd8331432dbfb8e2133197bbd3defcd44fa3587
KailanySousa <kailany-sousa2010@hotmail.com>
2025-08-09 21:14:26 -0300

    refactor: extrai elementos do SignalsIntroComponent para um arquivo a parte

--- src/app/signals-intro/signals-intro.component.ts
+++ src/app/signals-intro/signals-intro.component.ts
@@ -1,10 +1,5 @@
 import { Component, computed, signal } from '@angular/core';
-
-interface Elemento {
-  nome: string;
-  simbolo: string;
-  numeroMassa: number;
-}
+import Elemento, { ELEMENTOS } from './signals-intro.const';
 
 @Component({
   selector: 'app-signals-intro',
@@ -14,14 +9,7 @@ interface Elemento {
 export class SignalsIntroComponent {
   elementoSelecionado = signal<Elemento | null>(null);
 
-  elementos: Elemento[] = [
-    { nome: 'Hidrogênio', simbolo: 'H', numeroMassa: 1 },
-    { nome: 'Carbono', simbolo: 'C', numeroMassa: 12 },
-    { nome: 'Nitrogênio', simbolo: 'N', numeroMassa: 14 },
-    { nome: 'Oxigênio', simbolo: 'O', numeroMassa: 16 },
-    { nome: 'Sódio', simbolo: 'Na', numeroMassa: 23 },
-    { nome: 'Cloro', simbolo: 'Cl', numeroMassa: 35 },
-  ];
+  elementos: Elemento[] = ELEMENTOS;
 
   selecionarElemento(elemento: Elemento) {
     this.elementoSelecionado.set(elemento);
