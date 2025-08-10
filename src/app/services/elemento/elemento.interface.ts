a57373a66d0e13771c3030b8d90c207b2ab3a0c9
KailanySousa <kailany-sousa2010@hotmail.com>
2025-08-09 22:02:17 -0300

    feat: cria service para centralizar código

---
+++ src/app/services/elemento/elemento.interface.ts
@@ -0,0 +1,7 @@
+export default interface Elemento {
+  nome: string;
+  simbolo: string;
+  numeroMassa: number;
+  pontoFusao: number;
+  pontoEbulicao: number;
+}
---
+++ src/app/services/elemento/elemento.service.ts
@@ -0,0 +1,103 @@
+import { computed, effect, Injectable, signal } from '@angular/core';
+import Elemento from './elemento.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ElementoService {
+  elementoSelecionado = signal<Elemento | null>(null);
+  temperatura = signal<number>(25);
+  estadoFisico = signal<string>('');
+
+  elementos: Elemento[] = [
+    {
+      nome: 'Hidrogênio',
+      simbolo: 'H',
+      numeroMassa: 1,
+      pontoFusao: -259,
+      pontoEbulicao: -253,
+    },
+    {
+      nome: 'Carbono',
+      simbolo: 'C',
+      numeroMassa: 12,
+      pontoFusao: 3550,
+      pontoEbulicao: 4027,
+    },
+    {
+      nome: 'Nitrogênio',
+      simbolo: 'N',
+      numeroMassa: 14,
+      pontoFusao: -210,
+      pontoEbulicao: -196,
+    },
+    {
+      nome: 'Oxigênio',
+      simbolo: 'O',
+      numeroMassa: 16,
+      pontoFusao: -218,
+      pontoEbulicao: -183,
+    },
+    {
+      nome: 'Sódio',
+      simbolo: 'Na',
+      numeroMassa: 23,
+      pontoFusao: 98,
+      pontoEbulicao: 883,
+    },
+    {
+      nome: 'Cloro',
+      simbolo: 'Cl',
+      numeroMassa: 35,
+      pontoFusao: -101,
+      pontoEbulicao: -34,
+    },
+  ];
+
+  constructor() {
+    effect(
+      () => {
+        const elemento = this.elementoSelecionado();
+
+        if (elemento) {
+          let estadoFisico = this.verificaEstadoFisico(elemento);
+          this.estadoFisico.set(estadoFisico);
+        }
+      },
+      { allowSignalWrites: true }
+    );
+  }
+
+  private verificaEstadoFisico(elemento: Elemento) {
+    const temperatura = this.temperatura();
+
+    let estadoFisico = '';
+    if (temperatura < elemento.pontoFusao) {
+      estadoFisico = 'Sólido';
+    } else if (
+      temperatura >= elemento.pontoFusao &&
+      temperatura < elemento.pontoEbulicao
+    ) {
+      estadoFisico = 'Líquido';
+    } else {
+      estadoFisico = 'Gasoso';
+    }
+    return estadoFisico;
+  }
+
+  // sinal computado: valores derivados de outros sinais
+  elementoInfo = computed(() => {
+    const elemento = this.elementoSelecionado();
+    return elemento
+      ? `Nome: ${elemento.nome}, Símbolo: ${elemento.simbolo}, Número de massa: ${elemento.numeroMassa}`
+      : 'Nenhum elemento selecionado';
+  });
+
+  selecionarElemento(elemento: Elemento) {
+    this.elementoSelecionado.set(elemento);
+  }
+
+  ajustarTemperatura(temperatura: number) {
+    this.temperatura.set(temperatura);
+  }
+}
