382cc3f25f73a3ca84a1dab2bbe30f0f53118bc6
KailanySousa <kailany-sousa2010@hotmail.com>
2025-08-09 22:38:49 -0300

    feat: otimiza componentes com OnPush

--- src/app/components/element-list/element-list.component.ts
+++ src/app/components/element-list/element-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import Elemento from 'src/app/services/elemento/elemento.interface';
 import { ElementoService } from 'src/app/services/elemento/elemento.service';
 
@@ -6,6 +6,8 @@ import { ElementoService } from 'src/app/services/elemento/elemento.service';
   selector: 'app-element-list',
   templateUrl: './element-list.component.html',
   styleUrls: ['./element-list.component.css'],
+  // Essa estratégia otimiza as atualizações dentro dos componentes, garantindo que elas ocorram apenas quando realmente houver novos dados
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ElementListComponent {
   constructor(readonly elementoService: ElementoService) {}
